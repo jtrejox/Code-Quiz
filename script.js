227e6188409cb116d98f396c0ca96f3f1a8668d3
Jorge Trejo <jtrejo@mail.com>
2020-11-06 19:16:53 -0800

    created questions interface

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ var buttonEl = document.getElementById("start_button")
 var bodyEl = document.querySelector("body")
 var secondsLeft = 70
 var questions = [{q:"What JavaScript method do we use to change an attribute in our HTML code?",
-                  a:".setAttribute"},
-                  {q:"what JavaScript method would we use to create a timer for a timed event?",
-                  a:".setInterval"  }
+                  a:".setAttribute", f1:".getAttribute", f2:".attribute", f3:".changeAttribute"},
+                //   {q:"what JavaScript method would we use to create a timer for a timed event?",
+                //   a:".setInterval"  }
                 ]
 
 // FUNCTIONS===============================================
@@ -22,31 +22,56 @@ function timer(){
     }, 1000)
 }
 
+function buttonsname(){
+
+
+
+}
+
+function buttons(){
+    for (let ans in questions[0]){
+        if (ans !== "q"){
+            var quest = questions[0][ans]
+            var buttonsDiv = document.getElementById("buttDiv")
+            var buttons = document.createElement("button")
+            
+            buttonsDiv.appendChild(buttons)
+            
+            buttons.textContent = quest
+        }
+    }
+}
+
 function addQuestions(){
     var div = document.createElement("div")
+    var intDiv = document.createElement("div")
     var question = document.createElement("h1")
-    var buttons = document.createElement("button")
     var qdiv = document.getElementById("qdiv")
     qdiv.appendChild(div)
     div.appendChild(question)
+    qdiv.appendChild(intDiv)
+    intDiv.setAttribute("class", "question")
+    intDiv.setAttribute("id", "buttDiv")
+    qdiv.setAttribute("class", "divs list")
     question.setAttribute("class", "big-text")
     question.textContent = questions[questionIndex].q
-}
+    buttons() 
+    // intDiv.appendChild(buttons)
+    }
+    
+
+    
+
+// }
 
 function quiz(){
     x = 1
     questionIndex = Math.floor(Math.random()* questions.length)
     bannerEl.setAttribute("style", "display:none;");
     var div = document.createElement("div")
-    // var question = document.createElement("h1")
-    // var buttons = document.createElement("button")
     bodyEl.appendChild(div)
-    // div.appendChild(div)
-    // div.appendChild(question)
     div.setAttribute("class", "divs list")
     div.setAttribute("id", "qdiv")
-    // question.setAttribute("class", "big-text")
-    // question.textContent = questions[questionIndex].q
     addQuestions()
 
 }
@@ -57,4 +82,4 @@ buttonEl.addEventListener("click", function(){
     timer()
     quiz()
 })
-console.log(bodyEl)
+// console.log(questions[0])
